Created computation examples and fixed calculation of P(i|arrival)

phph/model.py
@@ -34,7 +34,7 @@ class model:
         #precalculations
         self.__storeAllExitPhase()
         self.__storeAllProbPhasei()
-
+        
     def meanQueueLength(self):
         #returns the mean number of
         #customers that are waiting
@@ -180,25 +180,33 @@ class model:
         #process is currently in phase i
         return self.queue.arrivalExitRates[i,0]/-self.queue.arrivalGenerator[i,i]
 
-    def __probExitPhase(self,i):
-        #given that an arrival exits, returns
-        #the probability that the arrival came from
-        #phase i
-        d=0
-        for j in range(self.queue.nPhasesArrival()):
-            d += self.__probPhaseExit(j)*self.__probPhase(j)
-        return((self.__probPhaseExit(i)*self.__probPhase(i))/d)
-
     def __storeAllExitPhase(self):
-        self.allExitPhase = np.zeros((1,self.queue.nPhasesArrival()))
-        for i in range(self.queue.nPhasesArrival()):
-            self.allExitPhase[0,i] = self.__probExitPhase(i)
+        #returns the distribution (as a column vector) 
+        #of phases that the arrival exits from.
+        #Thus, each element in the distribution reflects
+        #the conditional probability that an arrival
+        #exited through phase i.  
+        
+        #create the embedded Markov chain of the PH generator
+        diag= abs(np.diag(self.queue.arrivalGenerator))
+        pmat = self.queue.arrivalGenerator / diag[:, np.newaxis]
+        pmat = pmat + np.identity(self.queue.nPhasesArrival())
+        #create a diagonal matrix of exit probabilities 
+        evec = 1-np.sum(pmat,axis=1)
+        pmat2 = np.zeros((self.queue.nPhasesArrival(),self.queue.nPhasesArrival()))
+        np.fill_diagonal(pmat2,evec)
+        
+        #evaluate (y,allExitPhase) = (y,allExitPhase)*[P,Pdiag] until initial distribution (y) is zero
+        y = np.copy(self.queue.arrivalInitDistribution)
+        while (np.sum(y)>self.eps):
+            self.allExitPhase = np.matmul(y,pmat2)
+            y = np.matmul(y,pmat)
     
-    def __storeAllProbPhasei(self):
+    def __storeAllProbPhasei(self):        
         self.allProbPhasei = np.zeros((1,self.queue.nPhasesArrival()))
         for i in range(self.queue.nPhasesArrival()):
             self.allProbPhasei[0,i] = self.__probPhase(i)
-        
+
     def localStateDist(self,k):
         #returns the local state distribution
         #of level k

queue_example.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+#In this example we replicate results from de Smit (1983)
+#and Ramaswami & Lucantoni (1985).
+
+#de Smit, J. H. A. (1983). Numerical solution for the multi-server queue with
+#hyper-exponential service times. Operations Research Letters, 2(5), 217–224.
+
+#Ramaswami, V., & Lucantoni, D. M. (1985). Algorithms for the multi-server
+#queue with phase type service. Communications in Statistics. Stochastic
+#Models, 1(3), 393–417. https://doi.org/10.1080/15326348508807020
+
 #--------------------------------
 #   PREAMPLE
 #--------------------------------
@@ -9,7 +19,67 @@ import numpy as np
 import phph
 
 #--------------------------------
-#   QUEUE PARAMETERS
+#   E_5 / H_2 / 15 
+#--------------------------------
+
+#Server capacity
+servers = 15
+
+#Arrival parameters
+
+#Initial distribution
+arrivalInitDistribution = np.matrix([[1.0,0.0,0.0,0.0,0.0]])
+
+#Phase-type generator
+arrivalGenerator = np.matrix([[-0.9*servers*5,0.9*servers*5,0.0,0.0,0.0],
+                              [0.0,-0.9*servers*5,0.9*servers*5,0.0,0.0],
+                              [0.0,0.0,-0.9*servers*5,0.9*servers*5,0.0],
+                              [0.0,0.0,0.0,-0.9*servers*5,0.9*servers*5],
+                              [0.0,0.0,0.0,0.0,-0.9*servers*5]])
+
+#Service parameters
+
+#Initial distribution
+serviceInitDistribution = np.matrix([[(24+math.sqrt(468))/54,
+                                      1-((24+math.sqrt(468))/54)]])
+
+#Phase-type generator
+serviceGenerator = np.matrix([[-3*serviceInitDistribution[0, 0],0.0],
+                              [0.0,-1.5*serviceInitDistribution[0, 1]]])
+
+#Create the model
+mdl = phph.model(arrivalInitDistribution,arrivalGenerator,
+                 serviceInitDistribution,serviceGenerator,
+                 servers)
+
+print("E_5 / H_2 / 15")
+
+#Expected waiting time, E[W] (actual)
+print(mdl.meanWaitingTime())
+#phph: 0.895427
+#de Smit: 0.8943
+#Ramaswami & Lucantoni: 0.89540
+
+#Probability of not waiting, P(W=0) (actual)
+print(1-mdl.probWait())
+#phph: 0.434633
+#de Smit: 0.4346
+#Ramaswami & Lucantoni: 0.434622
+
+#P(W>7.0550) P(W>4.1200) P(W>2.8563) (actual)
+print(mdl.waitDist(7.0550),mdl.waitDist(4.1200),mdl.waitDist(2.8563))
+#phph: 0.010000 0.049999 0.100001
+#de Smit: 0.01 0.05 0.10
+#Ramaswami & Lucantoni: NA
+
+#Expected number of customers in system, E[C] (continuous time)
+print(mdl.meanOccupancy())
+#phph: 25.58827
+#de Smit: 25.588
+#Ramaswami & Lucantoni: NA
+
+#--------------------------------
+#   E_2 / H_2 / 15 
 #--------------------------------
 
 #Server capacity
@@ -34,17 +104,147 @@ serviceInitDistribution = np.matrix([[(24+math.sqrt(468))/54,
 serviceGenerator = np.matrix([[-3*serviceInitDistribution[0, 0],0.0],
                               [0.0,-1.5*serviceInitDistribution[0, 1]]])
 
-
-#--------------------------------
-#   EVALUATE QUEUE
-#--------------------------------
-
+#Create the model
 mdl = phph.model(arrivalInitDistribution,arrivalGenerator,
                  serviceInitDistribution,serviceGenerator,
                  servers)
 
-print(1-mdl.probWait())
-print(mdl.meanWaitingTime())
-print(mdl.waitDist(3.0753,type="actual"))
-print(mdl.waitDist(3.0753,type="virtual"))
+print("E_2 / H_2 / 15")
 
+#Expected waiting time, E[W] (actual)
+print(mdl.meanWaitingTime())
+#phph: 0.977745
+#de Smit: 0.9777
+#Ramaswami & Lucantoni: 0.97773  
+
+#Probability of not waiting, P(W=0) (actual)
+print(1-mdl.probWait())
+#phph: 0.409896
+#de Smit: 0.4099
+#Ramaswami & Lucantoni: 0.409876
+
+#P(W>7.4924) P(W>4.4048) P(W>3.0753) (actual)
+print(mdl.waitDist(7.4924),mdl.waitDist(4.4048),mdl.waitDist(3.0753))
+#phph: 0.010000 0.049999 0.100001
+#de Smit: 0.01 0.05 0.10
+#Ramaswami & Lucantoni: NA
+
+#Expected number of customers in system, E[C] (continuous time)
+print(mdl.meanOccupancy())
+#phph: 26.69956
+#de Smit: 26.700
+#Ramaswami & Lucantoni: NA
+
+#--------------------------------
+#   M / H_2 / 15 
+#--------------------------------
+
+#Server capacity
+servers = 15
+
+#Arrival parameters
+
+#Initial distribution
+arrivalInitDistribution = np.matrix([[1.0]])
+
+#Phase-type generator
+arrivalGenerator = np.matrix([[-0.9*servers]])
+
+#Service parameters
+
+#Initial distribution
+serviceInitDistribution = np.matrix([[(24+math.sqrt(468))/54,
+                                      1-((24+math.sqrt(468))/54)]])
+
+#Phase-type generator
+serviceGenerator = np.matrix([[-3*serviceInitDistribution[0, 0],0.0],
+                              [0.0,-1.5*serviceInitDistribution[0, 1]]])
+
+#Create the model
+mdl = phph.model(arrivalInitDistribution,arrivalGenerator,
+                 serviceInitDistribution,serviceGenerator,
+                 servers)
+
+print("M / H_2 / 15")
+
+#Expected waiting time, E[W] (actual)
+print(mdl.meanWaitingTime())
+#phph: 1.11609 
+#de Smit: 1.1161
+#Ramaswami & Lucantoni: 1.11606
+
+#Probability of not waiting, P(W=0) (actual)
+print(1-mdl.probWait())
+#phph: 0.375117
+#de Smit: 0.3751
+#Ramaswami & Lucantoni: 0.375084
+
+#P(W>8.2209) P(W>4.8783) P(W>3.4390) (actual)
+print(mdl.waitDist(8.2209),mdl.waitDist(4.8783),mdl.waitDist(3.4390))
+#phph: 0.010000 0.050001 0.100002
+#de Smit: 0.01 0.05 0.10
+#Ramaswami & Lucantoni: NA
+
+#Expected number of customers in system, E[C] (continuous time)
+print(mdl.meanOccupancy())
+#phph: 28.56718
+#de Smit: 28.567
+#Ramaswami & Lucantoni: NA
+
+#--------------------------------
+#   H_2 / H_2 / 15 
+#--------------------------------
+
+#Server capacity
+servers = 15
+
+#Arrival parameters
+
+#Initial distribution
+arrivalInitDistribution = np.matrix([[0.90824829,1-0.90824829]])
+
+#Phase-type generator
+arrivalGenerator = np.matrix([[-24.52270384,0.0],
+                              [0.0,-2.477296157]])
+
+
+#Service parameters
+
+#Initial distribution
+serviceInitDistribution = np.matrix([[(24+math.sqrt(468))/54,
+                                      1-((24+math.sqrt(468))/54)]])
+
+#Phase-type generator
+serviceGenerator = np.matrix([[-3*serviceInitDistribution[0, 0],0.0],
+                              [0.0,-1.5*serviceInitDistribution[0, 1]]])
+
+#Create the model
+mdl = phph.model(arrivalInitDistribution,arrivalGenerator,
+                 serviceInitDistribution,serviceGenerator,
+                 servers)
+
+print("H_2 / H_2 / 15")
+
+#Expected waiting time, E[W] (actual)
+print(mdl.meanWaitingTime())
+#phph: 2.195077
+#de Smit: 2.1951
+#Ramaswami & Lucantoni: 2.19318  
+
+#Probability of not waiting, P(W=0) (actual)
+print(1-mdl.probWait())
+#phph: 0.242127
+#de Smit: 0.2421
+#Ramaswami & Lucantoni: 0.242164
+
+#P(W>13.7846) P(W>8.4791) P(W>6.1941) (actual)
+print(mdl.waitDist(13.7846),mdl.waitDist(8.4791),mdl.waitDist(6.1941))
+#phph: 0.010000 0.050000 0.100000 
+#de Smit: 0.01 0.05 0.10
+#Ramaswami & Lucantoni: NA
+
+#Expected number of customers in system, E[C] (continuous time)
+print(mdl.meanOccupancy())
+#phph: 43.13353
+#de Smit: 43.133
+#Ramaswami & Lucantoni: NA